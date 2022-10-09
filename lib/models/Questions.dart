d6627818ed9396a7483339b145aec90d7cb8c70f
Abhinavnair13 <92390306+Abhinavnair13@users.noreply.github.com>
2022-10-09 11:12:47 +0530

    Update Questions.dart

--- lib/models/Questions.dart
+++ lib/models/Questions.dart
@@ -9,9 +9,11 @@ class Question {
 const List sample_data = [
   {
     "id": 1,
-    "question":
-        "Flutter is an open-source UI software development kit created by ______",
-    "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
+    "question": "What's your gender",
+    "options": [
+      'Male',
+      'Female',
+    ],
     "answer_index": 1,
   },
   {
@@ -20,16 +22,5 @@ const List sample_data = [
     "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
     "answer_index": 2,
   },
-  {
-    "id": 3,
-    "question": "A memory location that holds a single letter or number.",
-    "options": ['Double', 'Int', 'Char', 'Word'],
-    "answer_index": 2,
-  },
-  {
-    "id": 4,
-    "question": "What command do you use to output data to the screen?",
-    "options": ['Cin', 'Count>>', 'Cout', 'Output>>'],
-    "answer_index": 2,
-  },
+  
 ];
