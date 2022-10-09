a32b946561981e188160acfe58ee03ca3fad46b1
Abhinavnair13 <92390306+Abhinavnair13@users.noreply.github.com>
2022-10-09 16:37:52 +0530

    Update score_screen.dart

--- lib/screens/score/score_screen.dart
+++ lib/screens/score/score_screen.dart
@@ -25,10 +25,17 @@ class ScoreScreen extends StatelessWidget {
               ),
               Spacer(),
               Text(
-                "${_qnController.correctAns * 10}/${_qnController.questions.length * 10}",
+                "Central Government Health Scheme (CGHS):",
                 style: Theme.of(context)
                     .textTheme
                     .headline4
+                    .copyWith(color: kSecondaryColor),
+              ),
+              Text(
+                "provides comprehensive health care facilities for central government officials and pensioners residing in cities. Operations of this scheme take place in cities such as Kolkata, Mumbai, Lucknow, Delphi, Nagpur, and Pune This scheme has the following main components: All dispensary related services including domiciliary care. In addition, the beneficiaries of this scheme have the privilege of being hospitalized each and every time they fall ill.",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1
                     .copyWith(color: kSecondaryColor),
               ),
               Spacer(flex: 3),
